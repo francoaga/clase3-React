940ac1f19c817199e1bcba1ba5d860e1647a9ea2
Franco Groselle <fandresgroselle@gmail.com>
2022-08-18 21:05:54 -0300

    estructura de un proyecto react

---
+++ src/Header.jsx
@@ -0,0 +1,14 @@
+import Footer from './Footer';
+
+export const greet= 'Hola Rolling';
+
+function Header() {
+    return(
+        <div>
+            <h2>Header</h2>
+            <p>Un dev se ocupa de este componente</p>
+        </div>
+    );
+}
+
+export default Header;
---
+++ src/Main.jsx
@@ -0,0 +1,10 @@
+const Main = () => {
+    return (
+        <div>
+            <h2>Main</h2>
+            <p>Un dev se ocupa de este otro componente</p>
+        </div>
+    );
+}
+
+export default Main;
